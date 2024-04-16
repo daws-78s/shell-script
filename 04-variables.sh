0ce294c982e5ad9192adaf55fecc05ec90ae2b10
joindevops <info@joindevops.com>
2024-04-16 07:43:24 +0530

    hello script

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #declaring of varible
-PERSON1=Sachin #no space between = and value
-PERSON2=Rahul
+PERSON1=$1
+PERSON2=$2
 
 #referring variable
 echo "$PERSON1:: Hello $PERSON2, How are you?"
