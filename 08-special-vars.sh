b4cb6bc92bb773493e72cde5cbcc1add6f63fc06
joindevops <info@joindevops.com>
2024-04-16 08:09:27 +0530

    hello script

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -8,4 +8,5 @@ echo "Home directory of current user: $HOME"
 echo "Which user is running this script: $USER"
 echo "Hostname: $HOSTNAME"
 echo "Process ID of the current shell script: $$"
-sleep 60
+sleep 60 &
+echo "Process ID of last background command: $!"
