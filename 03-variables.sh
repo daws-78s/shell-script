32fcccd488cae635e79116a55c8ef1a58c7fac88
joindevops <info@joindevops.com>
2024-04-16 07:54:37 +0530

    hello script

--- 03-variables.sh
+++ 03-variables.sh
@@ -6,6 +6,6 @@ PERSON2=Rahul
 
 #referring variable
 echo "$PERSON1:: Hello $PERSON2, How are you?"
-echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
+echo "${PERSON2}:: Hi $PERSON1, I am fine. How are you?"
 echo "$PERSON1:: I am fine too. how is your work?"
 echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps"
---
+++ 06-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MOVIES=("RRR" "DjTillu" "murari")
+
+# size of above array is 3.
+# index are 0,1,2
+
+# list always starts with 0.
+
+echo "First Movie is: ${MOVIES[0]} "
