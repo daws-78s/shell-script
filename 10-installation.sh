d28d368e54c57dfb115d05047000aefa23b04f8c
joindevops <info@joindevops.com>
2024-04-16 08:46:12 +0530

    hello script

--- 10-installation.sh
+++ 10-installation.sh
@@ -10,14 +10,24 @@ else
     echo "You are super user."
 fi
 
-dnf install mysqll -y
+dnf install mysql -y
 
 if [ $? -ne 0 ]
 then
     echo "Installation of mysql...FAILURE"
     exit 1
+else
+    echo "Installation of mysql...SUCCESS"
 fi
 
 dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of git...FAILURE"
+    exit 1
+else
+    Installation of Git...SUCCESS
+fi
 
 echo "is script proceeding?"
