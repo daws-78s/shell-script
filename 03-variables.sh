d9aef56a83bd091f9dabf9697252d0631d0ffa32
joindevops <info@joindevops.com>
2024-04-16 07:38:44 +0530

    hello script

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh #no space between = and value
+PERSON2=Suresh
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON1: I am fine too. how is your work?"
+echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps"
