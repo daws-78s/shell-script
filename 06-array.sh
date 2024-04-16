289541d8f84c94d89b6f240716bd176f39420549
joindevops <info@joindevops.com>
2024-04-16 07:55:55 +0530

    hello script

--- 06-array.sh
+++ 06-array.sh
@@ -7,4 +7,6 @@ MOVIES=("RRR" "DjTillu" "murari")
 
 # list always starts with 0.
 
-echo "First Movie is: ${MOVIES[0]} "
+echo "First Movie is: ${MOVIES[0]}"
+echo "First Movie is: ${MOVIES[1]}"
+echo "First Movie is: ${MOVIES[@]}"
