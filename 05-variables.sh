1c026b7d0fe93d45bd060aabb64c098bb214d659
joindevops <info@joindevops.com>
2024-04-16 07:47:18 +0530

    hello script

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter username::"
+
+read USERNAME #here USERNAME is variable
+
+echo "Please enter password::"
+
+read PASSWORD
+
+echo "Username is: $USERNAME, Password is: $PASSWORD"
