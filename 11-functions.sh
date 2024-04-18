7ca8a88565077d49223356705ec5a4686365d594
joindevops <info@joindevops.com>
2024-04-18 07:44:28 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$($0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE(){
    if [ $1 -ne 0 ]
@@ -20,8 +23,8 @@ else
     echo "You are super user."
 fi
 
-dnf install mysql -y
+dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing MySQL"
 
-dnf install git -y
-VALIDATE $? "Installing MySQL"
+dnf install git -y &>>$LOGFILE
+VALIDATE $? "Installing Git"
