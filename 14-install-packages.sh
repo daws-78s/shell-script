fcecc3d42aa774d74d68b988ce710ca76411f131
joindevops <info@joindevops.com>
2024-04-18 08:15:22 +0530

    functions

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi
+}
+
 if [ $USERID -ne 0 ]
 then
     echo "Please run this script with root access."
@@ -12,6 +26,13 @@ fi
 for i in $@
 do
     echo "package to install: $i"
+    dnf list installed $i &>>$LOGFILE
+    if [ $? -eq 0 ]
+    then
+        echo "$i already installed...SKIPPING"
+    else
+        echo "$i not installed...Need to install"
+    fi
 done
 
 
