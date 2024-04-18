4e8a9eb81025d00d24c41a69423f2cc427a655b0
joindevops <info@joindevops.com>
2024-04-18 07:31:04 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -3,8 +3,13 @@
 USERID=$(id -u)
 
 VALIDATE(){
-    echo "Exit status: $1"
-    echo "What are you doing: $2"
+   if [ $1 -ne 0 ]
+   then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi
 }
 
 if [ $USERID -ne 0 ]
