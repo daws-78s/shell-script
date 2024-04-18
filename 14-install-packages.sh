524ff7b7126664c8362a235e1206cba16c805f2d
joindevops <info@joindevops.com>
2024-04-18 08:18:19 +0530

    functions

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -12,10 +12,10 @@ N="\e[0m"
 VALIDATE(){
    if [ $1 -ne 0 ]
    then
-        echo "$2...FAILURE"
+        echo -e "$2...$R FAILURE $N"
         exit 1
     else
-        echo "$2...SUCCESS"
+        echo -e "$2...$G SUCCESS $N"
     fi
 }
 
@@ -35,7 +35,8 @@ do
     then
         echo -e "$i already installed...$Y SKIPPING $N"
     else
-        echo "$i not installed...Need to install"
+        dnf install $i -y &>>$LOGFILE
+        VALIDATE $? "Installation of $i"
     fi
 done
 
