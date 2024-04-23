bd5a5f9cecc4334fd039cba57843fdd7ad360883
joindevops <info@joindevops.com>
2024-04-23 07:52:47 +0530

    functions

---
+++ 17-set.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+dnf install mysfaffql -y
+dnf install git -y
+
+echo "is script proceeding?"
