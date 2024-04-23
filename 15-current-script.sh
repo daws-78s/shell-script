44572efd24b5007a08e8f040aee771688c8d1757
joindevops <info@joindevops.com>
2024-04-23 07:29:07 +0530

    functions

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -5,6 +5,6 @@ COURSE="DevOps from Current Script"
 echo "Before calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
 
-#./16-other-script.sh
+./16-other-script.sh
 
 echo "After calling other script, course: $COURSE"
---
+++ 16-other-script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+COURSE="DevOps from Other Script"
+
+echo "Variable value from Other script: $COURSE"
+echo "Process ID of other script: $$"
