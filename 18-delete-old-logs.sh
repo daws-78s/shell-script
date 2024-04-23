62cf5e5f9e4fa09f5083c8064520fcb1616a9893
joindevops <info@joindevops.com>
2024-04-23 08:19:17 +0530

    functions

---
+++ 18-delete-old-logs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SOURCE_DIRECTORY=/tmp/app-logs
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ -d $SOURCE_DIRECTORY ]
+then
+    echo -e "$G Source directory exists $N"
+else
+    echo -e "$R Please make sure $SOURCE_DIRECTORY exists $N"
+    exit 1
+fi
