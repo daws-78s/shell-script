a9db4410c10b0dbd73484352d24d08ef49e89d13
joindevops <info@joindevops.com>
2024-04-24 07:50:20 +0530

    functions

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -10,7 +10,7 @@ do
     FOLDER=$(echo $line | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
-        MESSAGE="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE"
+        MESSAGE+="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE"
     fi
 done <<< $DISK_USAGE
 
